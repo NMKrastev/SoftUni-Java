cfd23f85b0fceb52c4762fb0f1746eeba17a99bc
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2022-05-26 14:14:15 +0300

    PBExercises1516June2019

---
+++ Basics/FirstStepsInCoding/src/ProjectsCreation.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ProjectsCreation {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int projects = Integer.parseInt(scanner.nextLine());
+
+        System.out.printf("The architect %s will need %d hours to complete %d project/s.", name, projects * 3, projects);
+
+    }
+}
--- FirstStepsInCoding/src/HelloSoftUni.java
+++
@@ -1,8 +0,0 @@
-public class HelloSoftUni {
-
-    public static void main(String[] args) {
-
-        System.out.println("Hello SoftUni");
-
-    }
-}
