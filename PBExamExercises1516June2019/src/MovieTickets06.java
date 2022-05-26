567611385870df0d21cef8894ecf69109e927993
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2022-05-26 11:33:07 +0300

    PBExercises1516June2019

--- PBExamExercises1516June2019/src/MovieTickets06.java
+++ PBExamExercises1516June2019/src/MovieTickets06.java
@@ -8,17 +8,14 @@ public class MovieTickets06 {
         int n = Integer.parseInt(scanner.nextLine());
 
         for (int i = a1; i <= a2 - 1; i++) {
-            char check = (char) i;
-            if (check % 2 != 0) {
+            if (i % 2 != 0) {
                 for (int j = 1; j <= n - 1; j++) {
                     for (int k = 1; k <= n / 2 - 1; k++) {
 
-                        char ch = (char) i;
-                        int num = ch;
-                        String st = String.valueOf(ch);
+                        String letter = String.valueOf((char) i);
 
-                        if (i % 2 != 0 && ((j + k + ch) % 2 != 0)) {
-                            System.out.printf("%s-%d%d%d\n", st, j, k, num);
+                        if (i % 2 != 0 && ((j + k + i) % 2 != 0)) {
+                            System.out.printf("%s-%d%d%d\n", letter, j, k, i);
                         }
                     }
                 }
