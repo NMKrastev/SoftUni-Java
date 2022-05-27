22eb80edde256c0e377377e2cb2c4cd2d3ccbd4a
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2022-05-27 15:14:52 +0300

    PBExamExercises0607July2019

---
+++ Basics/PBExamExercises0607July2019/src/NameGame06.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class NameGame06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int maxScore = Integer.MIN_VALUE;
+        String winnerName = "";
+        String name = scanner.nextLine();
+
+
+        while (!name.equals("Stop")) {
+            int score = 0;
+            for (int i = 0; i < name.length(); i++) {
+
+                char symbol = name.charAt(i);
+                int number = Integer.parseInt(scanner.nextLine());
+                if (symbol == number) {
+                    score += 10;
+                } else {
+                    score += 2;
+                }
+
+            }
+            if (score >= maxScore) {
+                maxScore = score;
+                winnerName = name;
+            }
+            name = scanner.nextLine();
+        }
+
+        System.out.printf("The winner is %s with %d points!", winnerName, maxScore);
+    }
+}
