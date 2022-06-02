baa407029030027aa0cac653e1350decff4e6cab
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-02 13:03:01 +0300

    DataTypeAndVariables-Lab

--- Fundamentals/DataTypesАndVariables/src/PF06CharsToString.java
+++ Fundamentals/DataTypesАndVariables/src/PF06CharsToString.java
@@ -4,13 +4,10 @@ public class PF06CharsToString {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String result = "";
-        for (int i = 0; i < 3; i++) {
-            String input = scanner.nextLine();
-
-            result += input;
-        }
-        System.out.println(result);
+        char firstChar = scanner.nextLine().charAt(0);
+        char secondChar = scanner.nextLine().charAt(0);
+        char thirdChar = scanner.nextLine().charAt(0);
+        System.out.printf("%c%c%c", firstChar, secondChar, thirdChar);
     }
 }
 /*Write a program that reads 3 lines of input. On each line, you get a single character. Combine all the
