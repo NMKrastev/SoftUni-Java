8eb1cbecb5483cfac5aabf18317d041128d48f8f
Nikola <nikola.malinov.krastev@gmail.com>
2022-09-17 11:38:36 +0300

    Exercise A4

---
+++ Fundamentals/Exercise_BasicSyntaxConditionalStatementsAndLoops/src/A4_PrintAndSum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class A4_PrintAndSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numOne = Integer.parseInt(scanner.nextLine());
+        int numTwo = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+
+        for (int i = numOne; i <= numTwo ; i++) {
+            System.out.print(i + " ");
+            sum += i;
+        }
+        System.out.println();
+        System.out.printf("Sum: %d", sum);
+    }
+}
