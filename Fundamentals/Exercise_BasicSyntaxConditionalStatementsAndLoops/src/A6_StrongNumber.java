31753a47e40fa7dab93641fa72cfa9ab642fe754
Nikola <nikola.malinov.krastev@gmail.com>
2022-09-17 12:04:12 +0300

    Exercise A6

---
+++ Fundamentals/Exercise_BasicSyntaxConditionalStatementsAndLoops/src/A6_StrongNumber.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class A6_StrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+        int startNumber = number;
+        int totalSum = 0;
+
+        while (number > 0 ) {
+            int lastDigit = number % 10;
+            int fact = 1;
+            for (int i = 1; i <= lastDigit; i++) {
+                fact = fact * i;
+            }
+            totalSum += fact;
+
+            number = number / 10;
+        }
+
+        if (totalSum == startNumber) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+
+    }
+}
+/*Write a program to check whether or not a given number is strong.
+A number is strong if the sum of the Factorial of each digit is
+equal to the number. For example 145 is a strong number,
+because 1! + 4! + 5! = 145. Print "yes" if the number is strong
+and "no" if the number is not strong.*/
