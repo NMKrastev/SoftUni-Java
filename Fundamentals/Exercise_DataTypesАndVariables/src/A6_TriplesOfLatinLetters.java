518c71f523f19b3dbf48e393faa32e99b7bd3589
Nikola <nikola.malinov.krastev@gmail.com>
2022-09-23 10:02:36 +0300

    Exercise DataTypesAndVariables A6

---
+++ Fundamentals/Exercise_DataTypesАndVariables/src/A6_TriplesOfLatinLetters.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class A6_TriplesOfLatinLetters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            char firstLetter = (char) ('a' + i);
+            for (int j = 0; j < n; j++) {
+                char secondLetter = (char) ('a' + j);
+                for (int k = 0; k < n; k++) {
+                    char thirdLetter = (char) ('a' + k);
+                    System.out.printf("%c%c%c\n", firstLetter, secondLetter, thirdLetter);
+                }
+            }
+        }
+    }
+}
+/*Write a program to read an integer n and print all triples of the
+first n small Latin letters, ordered alphabetically*/
