511b8e5391d7a8d1ea0604aa3821f542d46ad7ca
Nikola <nikola.malinov.krastev@gmail.com>
2022-09-23 09:53:54 +0300

    Exercise DataTypesAndVariables A5

---
+++ Fundamentals/Exercise_DataTypesАndVariables/src/A5_PrintPartOfASCIITable.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class A5_PrintPartOfASCIITable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int startASCCI = Integer.parseInt(scanner.nextLine());
+        int endASCCI = Integer.parseInt(scanner.nextLine());
+
+        for (int i = startASCCI; i <= endASCCI ; i++) {
+
+            char currentSymbol = (char) i;
+            System.out.printf("%c ", currentSymbol);
+
+        }
+    }
+}
+/*Find online more information about ASCII (American Standard
+Code for Information Interchange) and write a program that prints
+part of the ASCII table of characters at the console. On the first
+line of input, you will receive the char index you should start
+with, and on the second line - the index of the last character
+you should print*/
