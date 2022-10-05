e4a3bd92f62940759959e24859265e7623faa5d1
Nikola <nikola.malinov.krastev@gmail.com>
2022-10-05 12:42:43 +0300

    Updated Methods Lab A7

--- Fundamentals/PF10-Methods/src/A7_RepeatString.java
+++ Fundamentals/PF10-Methods/src/A7_RepeatString.java
@@ -6,9 +6,9 @@ public class A7_RepeatString {
 
         String input = scanner.nextLine();
         int count = Integer.parseInt(scanner.nextLine());
-        String result = printDuplicatedString(input, count);
+        //String result = printDuplicatedString(input, count);
 
-        System.out.println(result);
+        System.out.println(printDuplicatedString(input, count));
     }
 
     private static String printDuplicatedString(String input, int count) {
