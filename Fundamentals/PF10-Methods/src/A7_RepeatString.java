7b96505c8bb10b0e17bf784b2f7f94d3127a528f
Nikola <nikola.malinov.krastev@gmail.com>
2022-10-05 12:38:51 +0300

    Added Methods Lab A7

---
+++ Fundamentals/PF10-Methods/src/A7_RepeatString.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class A7_RepeatString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        int count = Integer.parseInt(scanner.nextLine());
+        String result = printDuplicatedString(input, count);
+
+        System.out.println(result);
+    }
+
+    private static String printDuplicatedString(String input, int count) {
+
+        String result = "";
+
+        for (int i = 0; i < count; i++) {
+            result += input;
+        }
+
+        return result;
+    }
+}
+/*Write a method that receives a string and a repeat count n (integer).
+The method should return a new string (the old one repeated n times).*/
