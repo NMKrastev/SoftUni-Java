11e1be134785ae77a3f519925bb7163879957b35
Nikola <nikola.malinov.krastev@gmail.com>
2022-10-05 14:07:09 +0300

    Added Methods Exercise A1

---
+++ Fundamentals/PF11-Exercise_Methods/src/PF01_SmallestOfThreeNumbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PF01_SmallestOfThreeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int one = Integer.parseInt(scanner.nextLine());
+        int two = Integer.parseInt(scanner.nextLine());
+        int three = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(printSmallestNumber(one, two, three));
+
+    }
+
+    private static int printSmallestNumber(int one, int two, int three) {
+
+        if (one < two && one < three) {
+            return one;
+        } else if (two < one && two < three) {
+            return two;
+        }
+        return three;
+    }
+}
+/*Write a method to print the smallest of three integer numbers.
+Use an appropriate name for the method.*/
