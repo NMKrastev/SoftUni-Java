7d2541be9381e2092b23843a7af0ab5e2796c390
Nikola <nikola.malinov.krastev@gmail.com>
2022-10-13 11:33:50 +0300

    Added MoreExercise List A2

--- Fundamentals/PF15-MoreExercise_List/src/A2_CarRace.java
+++ Fundamentals/PF15-MoreExercise_List/src/A2_CarRace.java
@@ -56,3 +56,12 @@ public class A2_CarRace {
         return carOneTime;
     }
 }
+/*Write a program to calculate the winner of a car race. You will receive an array of numbers. Each array element
+represents the time needed to pass through that step (the index). There are going to be two cars. One of them starts
+from the left side, and the other one starts from the right side. The middle index of the array is the finish line.
+(The number of elements of the array will always be odd). Calculate the total time for each racer to reach the finish
+(the middle of the array) and print the winner with his total time. (The racer with less time). If you have a zero in
+the array, you must reduce the racer's time that reached it by 20% (from the time so far).
+Print the result in the following format "The winner is {left/right} with total time: {total time}",
+formatted with one digit after the decimal point.
+*/
