ab2bf7c237c02be43b725f3c8bba68609e0947d2
Nikola <nikola.malinov.krastev@gmail.com>
2022-10-24 11:00:24 +0300

    Added ObjectAndClasses Lab A5

---
+++ Fundamentals/PF17-ObjectsAndClasses/src/A5_Student.java
@@ -0,0 +1,39 @@
+public class A5_Student {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String hometown;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHometown(String hometown) {
+        this.hometown = hometown;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getHometown() {
+        return hometown;
+    }
+}
---
+++ Fundamentals/PF17-ObjectsAndClasses/src/A5_Students.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class A5_Students {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<A5_Student> studentInfo = new ArrayList<>();
+        String command = "";
+
+        while (!(command = scanner.nextLine()).equals("end")) {
+
+            String firstName = command.split(" ")[0];
+            String lastName = command.split(" ")[1];
+            int age = Integer.parseInt(command.split(" ")[2]);
+            String hometown = command.split(" ")[3];
+
+            A5_Student student = new A5_Student();
+
+            student.setFirstName(firstName);
+            student.setLastName(lastName);
+            student.setAge(age);
+            student.setHometown(hometown);
+
+            studentInfo.add(student);
+        }
+
+        String town = scanner.nextLine();
+
+        for (A5_Student student : studentInfo) {
+            if (student.getHometown().equals(town)) {
+
+                System.out.println(student.getFirstName() + " " + student.getLastName() + " is " + student.getAge() + " years old");
+
+            }
+        }
+    }
+}
+/*Define a class Student, which holds the following information about students: first name, last name, age, and hometown.
+Read the list of students until you receive the "end" command. After that, you will receive a city name. Print only
+students which are from the given city, in the following format: "{firstName} {lastName} is {age} years old".*/
