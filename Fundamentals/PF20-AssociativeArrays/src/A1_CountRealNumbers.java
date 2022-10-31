ace112a72cfb77e0f8f30e02f7a1fb99dc1dfec6
Nikola <nikola.malinov.krastev@gmail.com>
2022-10-31 10:30:24 +0200

    Added AssociativeArrays Lab A1

---
+++ Fundamentals/PF20-AssociativeArrays/src/A1_CountRealNumbers.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class A1_CountRealNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Double> numsList = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Double::parseDouble).collect(Collectors.toList());
+
+        Map<Double, Integer> countNumbersMap = new TreeMap<>();
+
+        for (double currentNum : numsList) {
+
+            Integer currentValue = countNumbersMap.get(currentNum);
+            if (countNumbersMap.containsKey(currentNum)) {
+                countNumbersMap.put(currentNum, currentValue + 1);
+            } else {
+                countNumbersMap.put(currentNum, 1);
+            }
+        }
+
+        for (Map.Entry<Double, Integer> entry : countNumbersMap.entrySet()) {
+            System.out.printf("%.0f -> %d\n", entry.getKey(), entry.getValue());
+        }
+    }
+}
+/*Read a list of real numbers and print them in ascending order along with their number of occurrences.*/
