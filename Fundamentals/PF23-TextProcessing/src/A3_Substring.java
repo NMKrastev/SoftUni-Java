f1cd779bdc5cea6391ee30ed9ffb7198c2bceb9c
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2022-11-07 10:04:55 +0200

    Added TextProcessing Lab A3

--- Fundamentals/PF23-TextProcessing/src/A3_Substring.java
+++ Fundamentals/PF23-TextProcessing/src/A3_Substring.java
@@ -1,8 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class A3_Substring {
     public static void main(String[] args) {
