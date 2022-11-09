6d210bd9c6df74f53ecff77d1b6aa19824aa4a71
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2022-11-09 20:44:58 +0200

    Updated A4

--- Fundamentals/PF23-TextProcessing/src/A4_TextFilter.java
+++ Fundamentals/PF23-TextProcessing/src/A4_TextFilter.java
@@ -7,19 +7,21 @@ public class A4_TextFilter {
         String[] wordsToRemove = scanner.nextLine().split(",\\s+");
         String text = scanner.nextLine();
 
-        for (int i = 0; i < wordsToRemove.length; i++) {
-            int index = text.indexOf(wordsToRemove[i]);
+        for (String banWord : wordsToRemove) {
             String asterisks = "";
-            for (int j = 0; j < wordsToRemove[i].length(); j++) {
-                asterisks += "*";
-            }
-
-            text = text.replace(wordsToRemove[i], asterisks);
-            index = text.indexOf(wordsToRemove[i]);
-
+            asterisks = getAsterisks(banWord, asterisks);
+            text = text.replace(banWord, asterisks);
         }
 
         System.out.println(text);
+    }
+
+    private static String getAsterisks(String banWord, String asterisks) {
+
+        for (int j = 0; j < banWord.length(); j++) {
+            asterisks += "*";
+        }
+        return asterisks;
     }
 }
 /*Write a program that takes a text and a string of banned words. All words included in the ban list should be replaced
