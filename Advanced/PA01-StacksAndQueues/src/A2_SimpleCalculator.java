37c72a0fd272480304721d31d0d66eb58e19d3db
Nikola <nikola.malinov.krastev@gmail.com>
2023-01-10 10:10:29 +0200

    Added A2

---
+++ Advanced/PA01-StacksAndQueues/src/A2_SimpleCalculator.java
@@ -0,0 +1,30 @@
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class A2_SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split("\\s+");
+        ArrayDeque<String> stack = new ArrayDeque<>();
+        Collections.addAll(stack, input);
+
+        while (stack.size() > 1) {
+            int firstNum = Integer.parseInt(stack.pop());
+            String operator = stack.pop();
+            int secondNum = Integer.parseInt(stack.pop());
+
+            if (operator.equals("+")) {
+                stack.push(String.valueOf(firstNum + secondNum));
+            } else {
+                stack.push(String.valueOf(firstNum - secondNum));
+            }
+        }
+
+        System.out.println(stack.pop());
+    }
+}
+/*Create a simple calculator that can evaluate simple expressions that will not hold any operator different from addition and subtraction. There will not be parentheses or operator precedence.
+Solve the problem using a Stack.
+*/
