ae6de9458cf14e6ddcd29c304178e766ece2cebe
Nikola <nikola.malinov.krastev@gmail.com>
2023-01-10 13:56:45 +0200

    Updated A1

--- Advanced/PA01-StacksAndQueues/src/A1_BrowserHistory.java
+++ Advanced/PA01-StacksAndQueues/src/A1_BrowserHistory.java
@@ -5,17 +5,15 @@ public class A1_BrowserHistory {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String input = scanner.nextLine();
+        String input;
         String currentUrl = null;
         ArrayDeque<String> urlStack = new ArrayDeque<>();
 
-        while (!input.equals("Home")) {
+        while (!(input = scanner.nextLine()).equals("Home")) {
 
             if (input.equals("back")) {
                 if (urlStack.isEmpty()) {
                     System.out.println("no previous URLs");
-                    input = scanner.nextLine();
-                    continue;
                 } else {
                     currentUrl = urlStack.pop();
                 }
@@ -27,8 +25,6 @@ public class A1_BrowserHistory {
             }
 
             System.out.println(currentUrl);
-
-            input = scanner.nextLine();
         }
     }
 }
