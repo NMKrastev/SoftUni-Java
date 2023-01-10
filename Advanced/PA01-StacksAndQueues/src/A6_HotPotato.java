4410f93b39ebb6b55d273816a5ac62ecc34814ac
Nikola <nikola.malinov.krastev@gmail.com>
2023-01-10 12:39:13 +0200

    Updated A6

--- Advanced/PA01-StacksAndQueues/src/A6_HotPotato.java
+++ Advanced/PA01-StacksAndQueues/src/A6_HotPotato.java
@@ -20,3 +20,9 @@ public class A6_HotPotato {
         System.out.println("Last is " + namesQueue.poll());
     }
 }
+/*Hot potato is a game in which children form a circle and start passing a hot potato.
+The counting starts with the first kid. Every nth toss, the child left with the potato leaves the game.
+When a kid leaves the game, it passes the potato forward. This continues repeating until there is only one kid left.
+Create a program that simulates the game of Hot Potato. Print every kid that is removed from the circle.
+In the end, print the kid that is left last.
+*/
