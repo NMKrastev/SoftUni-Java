ca33f30c0aa51b245b821e6443060c5c8444ce47
Nikola <nikola.malinov.krastev@gmail.com>
2023-01-10 09:53:29 +0200

    Added A1

--- Advanced/PA01-StacksAndQueues/src/A1_BrowserHistory.java
+++ Advanced/PA01-StacksAndQueues/src/A1_BrowserHistory.java
@@ -32,3 +32,9 @@ public class A1_BrowserHistory {
         }
     }
 }
+/*Write a program that takes two types of browser instructions:
+•	Normal navigation: a URL is set, given by a string;
+•	The string "back" sets the current URL to the last set URL
+After each instruction, the program should print the current URL. If the back instruction can't be executed, print
+"no previous URLs". The input ends with the "Home" command, and then you simply have to stop the program.
+*/
