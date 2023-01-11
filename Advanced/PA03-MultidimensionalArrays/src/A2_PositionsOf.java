bc90b69bad19677e72f2f41f310c115c4922a50d
Nikola <nikola.malinov.krastev@gmail.com>
2023-01-11 18:12:42 +0200

    Added A2

--- Advanced/PA03-MultidimensionalArrays/src/A2_PositionsOf.java
+++ Advanced/PA03-MultidimensionalArrays/src/A2_PositionsOf.java
@@ -30,3 +30,12 @@ public class A2_PositionsOf {
         }
     }
 }
+/*Write a program that reads a matrix of integers from the console, then a number, and prints all the
+positions at which that number appears in the matrix.
+The matrix definition on the console will contain a line with two positive integer numbers R and C –
+the number of rows and columns in the matrix – followed by R lines, each containing C numbers (separated by spaces),
+representing each row of the matrix.
+The number you will need to find the positions will be entered on a single line after the matrix.
+You should print each position on a single line – first print the row, then the column at which the number appears.
+If the number does not appear in the matrix, print "not found".
+*/
