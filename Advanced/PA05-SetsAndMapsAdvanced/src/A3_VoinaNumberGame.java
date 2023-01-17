e65da75e2f4b5f961b5a3ae213f96ecc7d63b10f
Nikola <nikola.malinov.krastev@gmail.com>
2023-01-17 20:19:55 +0200

    Updated A3

--- Advanced/PA05-SetsAndMapsAdvanced/src/A3_VoinaNumberGame.java
+++ Advanced/PA05-SetsAndMapsAdvanced/src/A3_VoinaNumberGame.java
@@ -7,40 +7,40 @@ public class A3_VoinaNumberGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-         LinkedHashSet<Integer> playerOne = Arrays.stream(scanner.nextLine().split("\\s+"))
-                 .map(Integer::parseInt).collect(Collectors.toCollection(LinkedHashSet::new));
+        LinkedHashSet<Integer> playerOne = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toCollection(LinkedHashSet::new));
         LinkedHashSet<Integer> playerTwo = Arrays.stream(scanner.nextLine().split("\\s+"))
                 .map(Integer::parseInt).collect(Collectors.toCollection(LinkedHashSet::new));
 
         int round = 50;
-         while (round-- > 0) {
+        while (round-- > 0) {
 
             int firstCard = playerOne.iterator().next();
             playerOne.remove(firstCard);
 
-             int secondCard = playerTwo.iterator().next();
-             playerTwo.remove(secondCard);
+            int secondCard = playerTwo.iterator().next();
+            playerTwo.remove(secondCard);
 
-             if (firstCard > secondCard) {
-                 playerOne.add(firstCard);
-                 playerOne.add(secondCard);
-             } else if (secondCard > firstCard) {
-                 playerTwo.add(firstCard);
-                 playerTwo.add(secondCard);
-             }
+            if (firstCard > secondCard) {
+                playerOne.add(firstCard);
+                playerOne.add(secondCard);
+            } else if (secondCard > firstCard) {
+                playerTwo.add(firstCard);
+                playerTwo.add(secondCard);
+            }
 
-             if (playerOne.isEmpty() || playerTwo.isEmpty()) {
-                 break;
-             }
-         }
+            if (playerOne.isEmpty() || playerTwo.isEmpty()) {
+                break;
+            }
+        }
 
-         if (playerOne.size() > playerTwo.size()) {
-             System.out.println("First player win!");
-         } else if (playerTwo.size() > playerOne.size()) {
-             System.out.println("Second player win!");
-         } else {
-             System.out.println("Draw!");
-         }
+        if (playerOne.size() > playerTwo.size()) {
+            System.out.println("First player win!");
+        } else if (playerTwo.size() > playerOne.size()) {
+            System.out.println("Second player win!");
+        } else {
+            System.out.println("Draw!");
+        }
     }
 }
 /*Write a program that:
