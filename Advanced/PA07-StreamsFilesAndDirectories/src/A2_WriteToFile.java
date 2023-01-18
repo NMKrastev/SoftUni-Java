f91385d96ea00df01a1ccc4860845a1880bf9c91
Nikola <nikola.malinov.krastev@gmail.com>
2023-01-18 13:12:23 +0200

    Added A2

--- Advanced/PA07-StreamsFilesAndDirectories/src/A2_WriteToFile.java
+++ Advanced/PA07-StreamsFilesAndDirectories/src/A2_WriteToFile.java
@@ -22,3 +22,5 @@ public class A2_WriteToFile {
         }
     }
 }
+/*Read the file named "input.txt" that is provided for this exercise and write all its content to a file
+while skipping any punctuation. Skip the following symbols: ",", ".", "!", "?".*/
