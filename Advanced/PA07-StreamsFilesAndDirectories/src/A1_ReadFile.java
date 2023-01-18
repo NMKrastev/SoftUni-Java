435247e16d0f9150896c762ab8e2211be113cc4e
Nikola <nikola.malinov.krastev@gmail.com>
2023-01-18 11:10:52 +0200

    Added A1 and resources

---
+++ Advanced/PA07-StreamsFilesAndDirectories/src/A1_ReadFile.java
@@ -0,0 +1,26 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class A1_ReadFile {
+
+    public static void main(String[] args) {
+
+        String path = "C:\\Users\\n.krastev\\Documents\\GitHub\\SoftUni-Java\\Advanced\\PA07-StreamsFilesAndDirectories\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(path);
+            int bytes = fileInputStream.read();
+
+            while (bytes != -1) {
+
+                System.out.print(Integer.toBinaryString(bytes) + " ");
+                bytes = fileInputStream.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
+/*You are given a file named "input.txt". Read and print all of its contents as a sequence of bytes
+(the binary representation of the ASCII code for each character) separated by a single comma. */
