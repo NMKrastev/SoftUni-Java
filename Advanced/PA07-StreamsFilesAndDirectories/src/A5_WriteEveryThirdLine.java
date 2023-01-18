8f01beafc2603449bedd09971a8fddd8fd7aca5f
Nikola <nikola.malinov.krastev@gmail.com>
2023-01-18 14:44:56 +0200

    Added A6

---
+++ Advanced/PA07-StreamsFilesAndDirectories/src/A5_WriteEveryThirdLine.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class A5_WriteEveryThirdLine {
+
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream fileInputStream = new FileInputStream("PA07-StreamsFilesAndDirectories/resources/input.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream));
+        PrintWriter printWriter = new PrintWriter(new FileOutputStream("PA07-StreamsFilesAndDirectories/resources/05.WriteEveryThirdLineOutput.txt"));
+
+        int lineCounter = 1;
+        String line = reader.readLine();
+        while (line != null)  {
+            line = reader.readLine();
+            lineCounter++;
+            if (lineCounter % 3 == 0) {
+                printWriter.println(line);
+            }
+        }
+
+        reader.close();
+        printWriter.close();
+    }
+}
+/*Read the file provided, named "input.txt" and write to another file all lines whose number is divisible by 3.
+Line numbers start from one.*/
