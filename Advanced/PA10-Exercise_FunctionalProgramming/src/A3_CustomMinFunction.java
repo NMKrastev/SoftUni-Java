dc587c2c6fbe9d45812a0106490e328ac0cc4f28
Nikola <nikola.malinov.krastev@gmail.com>
2023-01-21 12:27:38 +0200

    Added A3

--- Advanced/PA10-Exercise_FunctionalProgramming/src/A3_CustomMinFunction.java
+++ Advanced/PA10-Exercise_FunctionalProgramming/src/A3_CustomMinFunction.java
@@ -1,19 +1,20 @@
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class A3_CustomMinFunction {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Integer[] array = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).toArray(Integer[]::new);
-        Function<Integer[], Integer> readArray = data -> Arrays.stream(data).mapToInt(Integer::new).min().orElse(0);
+        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
 
-        System.out.println(readArray);
+        Function<int[], Integer> getMin = nums -> Collections.min(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+
+        System.out.println(getMin.apply(numbers));
     }
-
 }
 /*Write a simple program that reads a set of numbers from the console
 and finds the smallest of the numbers using a simple Function<Integer[], Integer>.*/
