91133027106a3f1cca1c71a50a0375106c235ba4
Nikola <nikola.malinov.krastev@gmail.com>
2023-01-21 12:10:35 +0200

    Added A2

--- Advanced/PA10-Exercise_FunctionalProgramming/src/A2_KnightsOfHonor.java
+++ Advanced/PA10-Exercise_FunctionalProgramming/src/A2_KnightsOfHonor.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
@@ -8,15 +9,11 @@ public class A2_KnightsOfHonor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] names = scanner.nextLine().split("\\s+");
+        List<String> names = Arrays.stream(scanner.nextLine().split("\\s+")).collect(Collectors.toList());
 
-        Consumer<String[]> printNames = p -> {
-            System.out.println(Arrays.stream(p)
-                    .map(w -> String.format("Sir %s", w))
-                    .collect(Collectors.joining(System.lineSeparator())));
-        };
+        Consumer<String> printNames = n -> System.out.printf("Sir %s\n", n);
 
-        printNames.accept(names);
+        names.forEach(printNames);
     }
 }
 /*Write a program that reads a collection of names as strings from the console
