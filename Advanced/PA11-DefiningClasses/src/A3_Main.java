332843dc8e494630a0456022ae0f5609d7ad5dc5
Nikola <nikola.malinov.krastev@gmail.com>
2023-01-23 12:59:48 +0200

    Updated names of the classes

--- Advanced/PA11-DefiningClasses/src/A3_Main.java
+++ Advanced/PA11-DefiningClasses/src/A3_Main.java
@@ -55,3 +55,19 @@ public class A3_Main {
         return function.apply(bankAccount, operation[2]);
     }
 }
+/*Create class BankAccount.
+The class should have private fields for:
+•	Id: int (Starts from 1 and increments for every new account)
+•	Balance: double
+•	Interest rate: double (Shared for all accounts. Default value: 0.02)
+The class should also have public methods for:
+•	setInterestRate(double interest): void (static)
+•	getInterest(int Years): double
+•	deposit(double amount): void
+Create a test client supporting the following commands:
+•	Create
+•	Deposit {Id} {Amount}
+•	SetInterest {Interest}
+•	GetInterest {ID} {Years}
+•	End
+*/
