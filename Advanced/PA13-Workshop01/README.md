8d586b022b2c7321ee6feff668ca1261647deb71
Nikola <nikola.malinov.krastev@gmail.com>
2023-01-26 13:38:01 +0200

    Updated Main and README

--- Advanced/PA13-Workshop01/README.md
+++ Advanced/PA13-Workshop01/README.md
@@ -18,10 +18,10 @@ It will have the following functionality:</br>
      * ```void add(int firstIndex, int secondIndex)``` - Adds element at the specific index, the element at this index gets shifted to the right alongside any following elements, increasing the size</br>
      * ```void forEach(Consumer<Integer> consumer)``` - Goes through each one of the elements in the list</br>
    * Additional functionality:
-     * ```int[] grow()``` - Each time the size reaches the length it grows twice the size.
-     * ```int size()``` - Returns the size of the list
-     * ```void isIndexInBounds(int index)``` - Returns IndexOutOfBoundsException if the index is not in the list boundaries
-     * ```int[] shrink()``` - Shrinks the list to double the size of it each time it size equals the length divided by 4
+     * ```int[] grow()``` - Each time the size reaches the length it grows twice the size.</br>
+     * ```int size()``` - Returns the size of the list</br>
+     * ```void isIndexInBounds(int index)``` - Returns IndexOutOfBoundsException if the index is not in the list boundaries</br>
+     * ```int[] shrink()``` - Shrinks the list to double the size of it each time it size equals the length divided by 4</br>
 </br>
      
 2. Custom Stack - ***To be developed...***</br>
@@ -33,3 +33,5 @@ It will have the following functionality:</br>
      *	```int pop()``` – Removes the last added element</br>
      *	```int peek()``` – Returns the last element in the stack without removing it</br>
      *	```void forEach(Consumer<Integer> consumer)``` – Goes through each of the elements in the stack</br>
+   * Additional functionality:
+     * ```int size()``` - Returns size of the stack
--- Advanced/PA13-Workshop01/src/Main.java
+++ Advanced/PA13-Workshop01/src/Main.java
@@ -8,7 +8,50 @@ public class Main {
         /**
          * SmartArray Start
          */
+        /*Using add() method of SmartArray class*/
+        //Doing for loop with 1000000 iterations takes between 25-29 milliseconds
+        List<Integer> list = new ArrayList<>();
+        long startTimeList = System.currentTimeMillis();
+        for (int i = 1; i <= 5 ; i++) {
+            list.add(i);
+        }
+        long endTimeList = System.currentTimeMillis();
+        System.out.println(endTimeList - startTimeList);
 
+        //Doing for loop with 1000000 iterations takes between 6-10 milliseconds
+        SmartArray smartArray = new SmartArray();
+        long startTimeArray = System.currentTimeMillis();
+        for (int i = 1; i <= 5 ; i++) {
+            smartArray.add(i);
+        }
+        long endTimeArray = System.currentTimeMillis();
+        System.out.println(endTimeArray - startTimeArray);
+
+        /*Using get() method of SmartArray class*/
+        System.out.println(smartArray.get(5 - 1)); //returns 5
+        //System.out.println(smartArray.get(5)); //returns IndexOutOfBoundsException
+        /**
+         * If you want to use the code below make sure to comment the line above!
+         */
+
+        /*Using size() method of SmartArray class*/
+        System.out.println(smartArray.size()); //returns 5
+        printArray(smartArray);  // returns [1, 2, 3, 4, 5]
+
+        /*Using remove() method of SmartArray class*/
+        System.out.println(smartArray.remove(2)); // returns 3
+        printArray(smartArray);  // returns [1, 2, 4, 5]
+
+        /*Using contains() method of SmartArray class*/
+        System.out.println(smartArray.contains(2)); //returns true
+        System.out.println(smartArray.contains(7)); //returns false
+
+        /*Using add() method with element on curtain index of SmartArray class*/
+        smartArray.add(2, 3); // ads element(number) 3 to index 2
+        printArray(smartArray); // returns [1, 2, 3, 4, 5]
+
+        /*Using forEach() method of SmartArray class*/
+        smartArray.forEach(System.out::print); // returns [1, 2, 3, 4, 5]
         /**
          * SmartArray End
          */
