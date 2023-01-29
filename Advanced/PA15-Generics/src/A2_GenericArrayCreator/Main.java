7373e6cfe54b33a95d68556b8ebf0ae010d81455
Nikola <nikola.malinov.krastev@gmail.com>
2023-01-29 17:31:44 +0200

    Add A2

--- Advanced/PA15-Generics/src/A2_GenericArrayCreator/Main.java
+++ Advanced/PA15-Generics/src/A2_GenericArrayCreator/Main.java
@@ -15,3 +15,8 @@ public class Main {
         }
     }
 }
+/*Create a class ArrayCreator with a method and a single overload to it:
+•	static T[] create(int length, T item)
+•	static T[] create(Class<T> class, int length, T item)
+The method should return an array with the given length, and every element should be set to the given default item.
+*/
