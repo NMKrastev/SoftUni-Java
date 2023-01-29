d167293e984a5f6f99541fde6bb5f31c5de54efc
Nikola <nikola.malinov.krastev@gmail.com>
2023-01-29 16:51:46 +0200

    Add A1

---
+++ Advanced/PA15-Generics/src/A1_JarOfT/Main.java
@@ -0,0 +1,22 @@
+package A1_JarOfT;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Jar<String> stringJar = new Jar<String>();
+
+        stringJar.add("Ivan");
+        stringJar.add("Peter");
+
+        System.out.println(stringJar.remove());
+
+    }
+}
+/*Create a class Jar<> that can store anything.
+It should have two public methods:
+•	void add(element)
+•	element remove()
+Adding should add on top of its contents. Remove should get the topmost element.
+Use the syntax Jar<T> to create a generic class.
+*/
