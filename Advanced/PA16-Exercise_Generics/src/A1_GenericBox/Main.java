f41a43391aefb3e34fdf67553d3837cf024c61cf
Nikola <nikola.malinov.krastev@gmail.com>
2023-01-30 11:22:52 +0200

    Added A1

---
+++ Advanced/PA16-Exercise_Generics/src/A1_GenericBox/Main.java
@@ -0,0 +1,24 @@
+package A1_GenericBox;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        Box<String> box;
+        for (int i = 0; i < n; i++) {
+            String input = scanner.nextLine();
+            box = new Box<>(input);
+            System.out.println(box);
+        }
+    }
+}
+/*Create a generic class Box that can store any type. Override the toString() method to print the type
+and the value of the stored data in the format "{class full name}: {value}".
+Use the class that you've created and test it with the class java.lang.String. On the first line, you will get n -
+the number of strings to read from the console. On the next n lines, you will get the actual strings.
+For each of them, create a box and call its toString() method to print its data on the console.
+*/
