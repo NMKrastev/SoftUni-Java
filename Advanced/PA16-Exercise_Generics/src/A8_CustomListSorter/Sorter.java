dce0546bf4ad963368a3d0d36ffc0c8d7e2c1a27
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2023-01-31 09:59:27 +0200

    Added A8

---
+++ Advanced/PA16-Exercise_Generics/src/A8_CustomListSorter/Sorter.java
@@ -0,0 +1,16 @@
+package A8_CustomListSorter;
+
+public class Sorter {
+
+    public static <T extends Comparable<T>> void sort(CustomList<T> customList) {
+        //Bubble sort
+        for (int i = 0; i < customList.size(); i++) {
+            T currentElement = customList.get(i);
+            for (int j = i + 1; j < customList.size(); j++) {
+                if (currentElement.compareTo(customList.get(j)) > 0) {
+                    customList.swap(i, j);
+                }
+            }
+        }
+    }
+}
