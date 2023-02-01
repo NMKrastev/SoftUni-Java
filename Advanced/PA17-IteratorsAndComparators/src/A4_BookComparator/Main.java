57223fe8e19be9f88a32b3445f5b0d2140d8b459
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2023-02-01 13:21:02 +0200

    Added A4

--- Advanced/PA17-IteratorsAndComparators/src/A4_BookComparator/Main.java
+++ Advanced/PA17-IteratorsAndComparators/src/A4_BookComparator/Main.java
@@ -24,3 +24,11 @@ public class Main {
 
     }
 }
+/*Create a class BookComparator from the UML diagram below:
+<<Comparator<Book>>>
+BookComparator
++	compare(Book, Book): int
+BookComparator has to compare two books by:
+1.	Book title.
+2.	Year of publishing a book.
+*/
