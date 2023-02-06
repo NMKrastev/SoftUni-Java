7b51e1d09773511d5061599aaf481810e5823fed
Nikola <nikola.malinov.krastev@gmail.com>
2023-02-06 11:08:46 +0200

    Exam Preparation June 2020

--- Advanced/PA19_ExamPreparation/src/EP3_JavaAdvancedExam28June2020/A3_Parking/Main.java
+++ Advanced/PA19_ExamPreparation/src/EP3_JavaAdvancedExam28June2020/A3_Parking/Main.java
@@ -44,3 +44,37 @@ public class Main {
 
     }
 }
+/*Your task is to create a repository, which stores items by creating the classes described below.
+Car
+First, write a Java class Car with the following fields:
+•	manufacturer: String
+•	model: String
+•	year: int
+The class constructor should receive the manufacturer, model, and year.
+You need to create the appropriate getters and setters. Override the toString() method in the following format:
+"{manufacturer} {model} ({year})"
+Parking
+Next, write a Java class Parking that has data (Collection, which stores the entity Car).
+All entities inside the repository have the same fields. Also, the Parking class should have those fields:
+•	type: String
+•	capacity: int
+The class constructor should receive type and capacity, also it should initialize the data with a new instance of the collection.
+Implement the following features:
+•	Field data – Collection that holds added cars
+•	Method add(Car car) – adds an entity to the data if there is an empty cell for the car.
+•	Method remove(String manufacturer, String model) – removes the car by given manufacturer and model, if such exists,
+and returns boolean.
+•	Method getLatestCar() – returns the latest car (by year) or null if have no cars.
+•	Method getCar(String manufacturer, String model) – returns the car with the given manufacturer and model
+or null if there is no such car.
+•	Getter getCount() – returns the number of cars.
+•	getStatistics() – returns a String in the following format:
+o	"The cars are parked in {parking type}:
+{Car1}
+{Car2}
+(…)"
+Constraints
+•	The combinations of manufacturers and models will be always unique.
+•	The year of the cars will always be positive.
+•	There won't be cars of the same year.
+*/
