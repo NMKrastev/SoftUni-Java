d622af231101dda14a839dbb650a5f5ae2daafeb
Nikola <nikola.malinov.krastev@gmail.com>
2023-02-13 10:23:26 +0200

    Added A3

---
+++ Advanced/PA19_ExamPreparation/src/EP11_JavaAdvancedRetakeExam15December2021/A3_WaterAdventure/Aquarium.java
@@ -0,0 +1,65 @@
+package EP11_JavaAdvancedRetakeExam15December2021.A3_WaterAdventure;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Aquarium {
+
+    private String name;
+    private int capacity;
+    private int size;
+    private List<Fish> fishInPool;
+
+    public Aquarium(String name, int capacity, int size) {
+        this.name = name;
+        this.capacity = capacity;
+        this.size = size;
+        this.fishInPool = new ArrayList<>();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getFishInPool() {
+        return fishInPool.size();
+    }
+
+    public void add(Fish fish) {
+        if (fishInPool.isEmpty()) {
+            fishInPool.add(fish);
+        } else {
+            for (Fish current : fishInPool) {
+                if (!current.getName().equals(fish.getName()) && fishInPool.size() < capacity) {
+                    fishInPool.add(fish);
+                    break;
+                }
+            }
+        }
+    }
+
+    public boolean remove(String name) {
+        return fishInPool.removeIf(e -> e.getName().equals(name));
+    }
+
+    public Fish findFish(String name) {
+        return fishInPool.stream()
+                .filter(e -> e.getName().equals(name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public String report() {
+        return String.format("Aquarium: %s ^ Size: %d\n%s", name, size, fishInPool.stream().map(String::valueOf).collect(Collectors.joining(System.lineSeparator()))).trim();
+    }
+}
---
+++ Advanced/PA19_ExamPreparation/src/EP11_JavaAdvancedRetakeExam15December2021/A3_WaterAdventure/Fish.java
@@ -0,0 +1,31 @@
+package EP11_JavaAdvancedRetakeExam15December2021.A3_WaterAdventure;
+
+public class Fish {
+
+    private String name;
+    private String color;
+    private int fins;
+
+    public Fish(String name, String color, int fins) {
+        this.name = name;
+        this.color = color;
+        this.fins = fins;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getFins() {
+        return fins;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Fish: %s\nColor: %s\nNumber of fins: %d", name, color, fins);
+    }
+}
