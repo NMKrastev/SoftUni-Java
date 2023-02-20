de08f603e6d0d41e6223ef6ee38bbc8f6b8349ad
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2023-02-20 13:53:49 +0200

    Added A2

---
+++ OOP/OOP3-Encapsulation/src/A3_ValidationData/Main.java
@@ -0,0 +1,49 @@
+package A3_ValidationData;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(reader.readLine());
+
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] input = reader.readLine().split("\\s+");
+
+            Optional<Person> person = Optional.empty();
+
+            try {
+                person = Optional.of(new Person
+                        (input[0], input[1], Integer.parseInt(input[2]), Double.parseDouble(input[3]))
+                );
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
+            person.ifPresent(people::add);
+        }
+
+        double bonus = Double.parseDouble(reader.readLine());
+
+        for (Person person : people) {
+            person.increaseSalary(bonus);
+            System.out.println(person);
+        }
+    }
+}
+/*Expand Person with proper validation for every field:
+•	Names must be at least 3 symbols
+•	Age must not be zero or negative
+•	Salary can't be less than 460.0
+Print proper message to end-user (look at an example for messages).
+Don't use System.out.println() in Person class.
+*/
---
+++ OOP/OOP3-Encapsulation/src/A3_ValidationData/Person.java
@@ -0,0 +1,67 @@
+package A3_ValidationData;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setAge(age);
+        setSalary(salary);
+    }
+
+    private void setFirstName(String firstName) {
+        validateNameLength(firstName, "First");
+        this.firstName = firstName;
+    }
+
+    private void setLastName(String lastName) {
+        validateNameLength(lastName, "Last");
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    private void setAge(int age) {
+        if (age <= 0) {
+            throw new IllegalArgumentException("Age cannot be zero or negative integer");
+        }
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    private void setSalary(double salary) {
+        if (salary < 460) {
+            throw new IllegalArgumentException("Salary cannot be less than 460 leva");
+        }
+        this.salary = salary;
+    }
+
+    private void validateNameLength(String name, String messagePrefix) {
+        if (name.length() < 3) {
+            throw new IllegalArgumentException(String.format("%s name cannot be less than 3 symbols", messagePrefix));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s gets %.1f leva", firstName, lastName, salary);
+    }
+
+    public void increaseSalary(double bonus) {
+        if (getAge() < 30) {
+            setSalary(getSalary() + (getSalary() * bonus / 200));
+        } else {
+            setSalary(getSalary() + (getSalary() * bonus / 100));
+        }
+    }
+}
