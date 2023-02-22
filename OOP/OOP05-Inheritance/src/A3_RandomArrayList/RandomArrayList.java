8aff9418d4f2123e23186eef7a1d0bd2fb19b15a
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2023-02-22 12:13:33 +0200

    Added A3

--- OOP/OOP05-Inheritance/src/A3_RandomArrayList/RandomArrayList.java
+++ OOP/OOP05-Inheritance/src/A3_RandomArrayList/RandomArrayList.java
@@ -1,23 +1,11 @@
 package A3_RandomArrayList;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomArrayList<T> extends ArrayList<T> {
 
-    List<T> list;
-
-    public RandomArrayList () {
-        this.list = new ArrayList<>();
-    }
     public Object getRandomElement() {
-        Random random = new Random();
-
-        return list.get(random.nextInt(list.size()));
-    }
-
-    public boolean add(T element) {
-        return list.add(element);
+        return get(ThreadLocalRandom.current().nextInt(0, size()));
     }
 }
