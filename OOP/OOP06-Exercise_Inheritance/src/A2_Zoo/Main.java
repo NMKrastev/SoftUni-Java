6a3272bc222826c547d86f95e4d1f3121e162c90
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2023-02-23 09:56:23 +0200

    Added A2

--- OOP/OOP06-Exercise_Inheritance/src/A2_Zoo/Main.java
+++ OOP/OOP06-Exercise_Inheritance/src/A2_Zoo/Main.java
@@ -10,3 +10,13 @@ public class Main {
 
     }
 }
+/*NOTE: You need a public class Main.
+Create a package zoo. It needs to contain the following classes:
+                        Animal
+          Reptile                      Mammal
+    Lizard       Snake          Gorilla      Bear
+Follow the diagram and create all of the classes. Each of them, except the Animal class,
+should inherit from another class. The Animal class should have a field name – String and Getter for a name.
+Every class should have:
+•	A public constructor, which accepts one parameter: name
+*/
