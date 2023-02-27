6dc0a5a4d3123e84be7f5ff60d8b298ee970b9b0
Nikola <nikola.malinov.krastev@gmail.com>
2023-02-27 23:38:27 +0200

    Added A4

--- OOP/OOP09-Polymorphism/src/A4_WildFarm/Main.java
+++ OOP/OOP09-Polymorphism/src/A4_WildFarm/Main.java
@@ -77,3 +77,24 @@ public class Main {
         return food;
     }
 }
+/*Your task is to create a class hierarchy like the picture below.
+All the classes except Vegetable, Meat, Mouse, Tiger, Cat & Zebra should be abstract.
+Input should be read from the console. Every even line will contain information about the Animal in following format:
+"{AnimalType} {AnimalName} {AnimalWeight} {AnimalLivingRegion}".
+If the animai is a cat: "{AnimalType} {AnimalName} {AnimalWeight} {AnimalLivingRegion} {CatBreed}".
+On the odd lines, you will receive information about the food that you should give to the Animal.
+The line will consist of FoodType and quantity separated by whitespace.
+You should build the logic to determine if the animal is going to eat the provided food.
+The Mouse and Zebra should check if the food is Vegetable. If it is they will eat it.
+Otherwise, you should print a message in the format:
+"{AnimalType} are not eating that type of food!". AnimalType to be in the plural.
+Cats eat any kind of food, but Tigers accept only Meat. If a Vegetable is provided to a tiger
+message like the one above should be printed on the console.
+After you read information about the Animal and Food then invoke makeSound() method
+of the current animal and then feed it. In the end, print the whole object in the format:
+"{AnimalType} [{AnimalName}, {AnimalWeight}, {AnimalLivingRegion}, {FoodEaten}]".
+If the animal is a cat: "{AnimalType} [{AnimalName}, {CatBreed}, {AnimalWeight}, {AnimalLivingRegion}, {FoodEaten}]".
+Proceed to read information about the next animal/food. The input will continue until you receive "End".
+Print all AnimalWeight with two digits after the decimal separator. Use the DecimalFormat class.
+Note: consider overriding toString() method.
+*/
