45a08327cedca0fd2e300b8cd20beb4bafc607bf
Nikola <nikola.malinov.krastev@gmail.com>
2023-03-12 15:43:45 +0200

    Added Exam Preparation 12 December 2020

--- OOP/OOP-ExamPreparation/src/test/java/JavaOOPExam_12December2020/bankSafe/BankVaultTest.java
+++ OOP/OOP-ExamPreparation/src/test/java/JavaOOPExam_12December2020/bankSafe/BankVaultTest.java
@@ -1,5 +1,7 @@
 package JavaOOPExam_12December2020.bankSafe;
 
+import JavaOOPExam_12December2020.bankSafe.BankVault;
+import JavaOOPExam_12December2020.bankSafe.Item;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,6 +11,13 @@ import java.util.Map;
 import static org.junit.Assert.*;
 
 public class BankVaultTest {
+
+    private static final String CELL_DOES_NOT_EXIST = "Cell doesn't exist!";
+    private static final String CELL_ALREADY_TAKEN = "Cell is already taken!";
+    private static final String ITEM_ALREADY_IN_CELL = "Item is already in cell";
+    private static final String ITEM_IN_CELL_DOES_NOT_EXIST = "Item in that cell doesn't exists!";
+    private static final String ITEM_SAVED = "Item:%s saved successfully!";
+    private static final String ITEM_REMOVED = "Remove item:%s successfully!";
 
     private Item itemOne = new Item("Ivan", "1");
     private Item itemTwo = new Item("Peter", "2");
@@ -32,49 +41,69 @@ public class BankVaultTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testAddItemShouldThrowIfCellDoesNotExist() throws OperationNotSupportedException, IllegalArgumentException {
-        this.bankVault.addItem(NON_EXISTENT_CELL, itemOne);
+        try {
+            this.bankVault.addItem(NON_EXISTENT_CELL, itemOne);
+        } catch (IllegalArgumentException e) {
+            assertEquals(CELL_DOES_NOT_EXIST, e.getMessage());
+            throw e;
+        }
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testAddItemShouldThrowIfCellIsAlreadyTaken() throws OperationNotSupportedException, IllegalArgumentException {
         this.bankVault.addItem(CELL, itemOne);
-        this.bankVault.addItem(CELL, itemTwo);
+        try {
+            this.bankVault.addItem(CELL, itemTwo);
+        } catch (IllegalArgumentException e) {
+            assertEquals(CELL_ALREADY_TAKEN, e.getMessage());
+            throw e;
+        }
     }
 
     @Test(expected = OperationNotSupportedException.class)
     public void testAddItemShouldThrowIfItemAlreadyExist() throws OperationNotSupportedException, IllegalArgumentException {
         this.bankVault.addItem(CELL, itemOne);
-        this.bankVault.addItem("A2", itemOne);
+        try {
+            this.bankVault.addItem("A2", itemOne);
+        } catch (OperationNotSupportedException e) {
+            assertEquals(ITEM_ALREADY_IN_CELL, e.getMessage());
+            throw e;
+        }
     }
 
     @Test
     public void testAddItemShouldPutItemToCell() throws OperationNotSupportedException, IllegalArgumentException {
-        this.bankVault.addItem(CELL, itemOne);
+        assertEquals(String.format(ITEM_SAVED, itemOne.getItemId()), this.bankVault.addItem(CELL, itemOne));
+        String expectedOwner = this.bankVault.getVaultCells().get(CELL).getOwner();
+        assertEquals(expectedOwner, this.bankVault.getVaultCells().get(CELL).getOwner());
         Item expected = this.bankVault.getVaultCells().get(CELL);
         assertEquals(expected, this.bankVault.getVaultCells().get(CELL));
     }
 
-    @Test
-    public void testAddItemShouldReturnCorrectOwner() throws OperationNotSupportedException, IllegalArgumentException {
-        this.bankVault.addItem(CELL, itemOne);
-        String expectedOwner = this.bankVault.getVaultCells().get(CELL).getOwner();
-        assertEquals(expectedOwner, this.bankVault.getVaultCells().get(CELL).getOwner());
-    }
-
     @Test(expected = IllegalArgumentException.class)
     public void testRemoveItemShouldThrowIfCellDoesntExist() {
-        this.bankVault.removeItem(NON_EXISTENT_CELL, itemOne);
+        try {
+            this.bankVault.removeItem(NON_EXISTENT_CELL, itemOne);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Cell doesn't exists!", e.getMessage());
+            throw e;
+        }
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testRemoveItemShouldThrowIfItemInCellDoesNotExist() {
-        this.bankVault.removeItem(CELL, itemOne);
+        try {
+            this.bankVault.removeItem(CELL, itemOne);
+        } catch (IllegalArgumentException e) {
+            assertEquals(ITEM_IN_CELL_DOES_NOT_EXIST, e.getMessage());
+            throw e;
+        }
     }
 
     @Test
     public void testRemoveItemShouldWork() throws OperationNotSupportedException, IllegalArgumentException {
         this.bankVault.addItem(CELL, itemOne);
-        this.bankVault.removeItem(CELL, itemOne);
+        assertEquals(String.format(ITEM_REMOVED, itemOne.getItemId()), this.bankVault.removeItem(CELL, itemOne));
         assertNull(this.bankVault.getVaultCells().get(CELL));
     }
 }
