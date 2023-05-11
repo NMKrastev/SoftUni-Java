97ab958ae69f91140c821ecbba0cbc6046a84f3a
Nikola <nikola.malinov.krastev@gmail.com>
2023-05-11 18:07:07 +0300

    Added A2

---
+++ MySQL/MySQL04-Exercise_BasicCRUD/A2_FindAllDepartmentNames.sql
@@ -0,0 +1,4 @@
+#Write SQL query to find all department names. Sort the information by id.
+#Submit your query statements as Prepare DB & run queries.
+SELECT `name` FROM `departments`
+ORDER BY `department_id`;
