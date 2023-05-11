de24ca7d59414ee50a59ebd89bdfb6256b45597e
Nikola <nikola.malinov.krastev@gmail.com>
2023-05-11 18:01:07 +0300

    Added A1

---
+++ MySQL/MySQL04-Exercise_BasicCRUD/A1_FindAllInformationAboutDepartments.sql
@@ -0,0 +1,4 @@
+#Write a SQL query to find all available information about the departments.
+#Sort the information by id. Submit your query statements as Prepare DB & run queries.
+SELECT * FROM `departments`
+ORDER BY `department_id`;
