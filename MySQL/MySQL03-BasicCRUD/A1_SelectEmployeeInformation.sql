0030048ae14d9c2bd4fa78c40f59bbdaf473380d
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2023-05-11 08:10:06 +0300

    Added A1

---
+++ MySQL/MySQL03-BasicCRUD/A1_SelectEmployeeInformation.sql
@@ -0,0 +1,4 @@
+#Write a query to select all employees and retrieve information about their 
+#id, first_name, last_name and job_title ordered by id.
+SELECT `id`, `first_name`, `last_name`, `job_title`
+FROM `employees`;
