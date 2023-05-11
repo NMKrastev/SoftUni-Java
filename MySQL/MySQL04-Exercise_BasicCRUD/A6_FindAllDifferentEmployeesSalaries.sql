dec66baa5bde88f49be71cc9c6ff59e3a8b50fb1
Nikola <nikola.malinov.krastev@gmail.com>
2023-05-11 18:44:58 +0300

    Added A6

---
+++ MySQL/MySQL04-Exercise_BasicCRUD/A6_FindAllDifferentEmployeesSalaries.sql
@@ -0,0 +1,4 @@
+#Write a SQL query to find all different employee's salaries.
+#Show only the salaries. Submit your query statements as Prepare DB & run queries.
+SELECT DISTINCT salary
+FROM employees;
