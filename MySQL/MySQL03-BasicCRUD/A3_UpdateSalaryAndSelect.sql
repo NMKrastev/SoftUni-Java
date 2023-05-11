b41a90fa920e15056a106c7575441c7f24af2717
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2023-05-11 09:14:10 +0300

    Added A3

--- MySQL/MySQL03-BasicCRUD/A3_UpdateSalaryAndSelect.sql
+++ MySQL/MySQL03-BasicCRUD/A3_UpdateSalaryAndSelect.sql
@@ -1,3 +1,8 @@
 #Update all employees' salaries whose job_title is "Manager" by adding 100.
 #Retrieve information about salaries from table employees.
-UPDATE em
+UPDATE `employees`
+SET `salary` = salary + 100
+WHERE `job_title` = 'Manager';
+
+SELECT `salary`
+FROM employees;
