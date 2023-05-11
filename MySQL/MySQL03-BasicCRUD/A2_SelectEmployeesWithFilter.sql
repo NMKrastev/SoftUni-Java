d074b907f641a3f3fb7ff99c525c8acb7ee8afa6
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2023-05-11 08:39:58 +0300

    Added A2

--- MySQL/MySQL03-BasicCRUD/A2_SelectEmployeesWithFilter.sql
+++ MySQL/MySQL03-BasicCRUD/A2_SelectEmployeesWithFilter.sql
@@ -1,5 +1,5 @@
 #Write a query to select all employees (id, first_name and last_name (as full_name), job_title, salary)
 #whose salaries are higher than 1000.00, ordered by id. Concatenate fields first_name and last_name into 'full_name'.
-SELECT `id`, CONCAT(`first_name`, ' ', `last_name`) AS `full_name`, `job_title`, `salary`
+SELECT `id`, CONCAT_WS(' ', `first_name`, `last_name`) AS `full_name`, `job_title`, `salary`
 FROM employees
 WHERE salary > 1000.0 ORDER BY id;
---
+++ MySQL/MySQL03-BasicCRUD/A3_UpdateSalaryAndSelect.sql
@@ -0,0 +1,2 @@
+#Update all employees' salaries whose job_title is "Manager" by adding 100.
+#Retrieve information about salaries from table employees.
