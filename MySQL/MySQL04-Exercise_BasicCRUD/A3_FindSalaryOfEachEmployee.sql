c59ad18bd9108fb0f106bb46792ebd9f0997c219
Nikola <nikola.malinov.krastev@gmail.com>
2023-05-11 18:11:51 +0300

    Added A3

---
+++ MySQL/MySQL04-Exercise_BasicCRUD/A3_FindSalaryOfEachEmployee.sql
@@ -0,0 +1,4 @@
+#Write SQL query to find the first name, last name and salary of each employee.
+#Sort the information by id. Submit your query statements as Prepare DB & run queries.
+SELECT `first_name`, `last_name`, `salary` FROM `employees`
+ORDER BY `employee_id`;
