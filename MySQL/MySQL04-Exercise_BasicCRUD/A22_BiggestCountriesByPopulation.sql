40ab43a9cc4c704982100e73a0293febf638d76b
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2023-05-12 09:22:18 +0300

    Added A22

---
+++ MySQL/MySQL04-Exercise_BasicCRUD/A22_BiggestCountriesByPopulation.sql
@@ -0,0 +1,8 @@
+#Find the 30 biggest countries by population from Europe. Display the country name and population.
+#Sort the results by population (from biggest to smallest), then by country alphabetically.
+#Submit your query statements as Prepare DB & run queries.
+SELECT `country_name`, `population`
+FROM `countries`
+WHERE `continent_code` = "EU"
+ORDER BY `population` DESC
+LIMIT 30;
