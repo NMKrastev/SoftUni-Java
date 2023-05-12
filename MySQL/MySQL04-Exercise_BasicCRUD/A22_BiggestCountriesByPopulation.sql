7565c7b042999e90b4184e411871d032bc43be9c
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2023-05-12 09:22:39 +0300

    Added A22

--- MySQL/MySQL04-Exercise_BasicCRUD/A22_BiggestCountriesByPopulation.sql
+++ MySQL/MySQL04-Exercise_BasicCRUD/A22_BiggestCountriesByPopulation.sql
@@ -3,6 +3,6 @@
 #Submit your query statements as Prepare DB & run queries.
 SELECT `country_name`, `population`
 FROM `countries`
-WHERE `continent_code` = "EU"
+WHERE `continent_code` = 'EU'
 ORDER BY `population` DESC
 LIMIT 30;
