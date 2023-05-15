88b4eb3a317dd7f5cf78d4da77186b8f96d01b15
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2023-05-15 10:52:17 +0300

    Added A1

---
+++ MySQL/MySQL05-Built-InFunctions/A1_FindBookTitles.sql
@@ -0,0 +1,5 @@
+#Write a SQL query to find books which titles start with "The".
+# Order the result by id. Submit your query statements as Prepare DB & run queries.
+SELECT `title`
+FROM books
+WHERE SUBSTR(`title`, 1, 3) = 'The';
