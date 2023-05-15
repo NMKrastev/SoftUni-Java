b8f262cf3cff2663a7d4e993d3445403da1fb1e9
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2023-05-15 11:53:39 +0300

    Added A4

--- MySQL/MySQL05-Built-InFunctions/A4_DaysLived.sql
+++ MySQL/MySQL05-Built-InFunctions/A4_DaysLived.sql
@@ -3,5 +3,5 @@
 #•	Days Lived – days that he/she lived. NULL values mean that the author is still alive.
 #Submit your query statements as Prepare DB & run queries.
 SELECT CONCAT_WS(' ', `first_name`, `last_name`) AS `Full Name`,
-       TIMESTAMPDIFF(DAY, `born`, `died`) AS 'Days Lived'
+       TIMESTAMPDIFF(DAY, `born`, `died`) AS `Days Lived`
 FROM `authors`;
