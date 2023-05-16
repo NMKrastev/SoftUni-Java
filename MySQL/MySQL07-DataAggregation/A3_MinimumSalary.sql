205ad58c323ac4e7eccc3f476ade95a205dffaae
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2023-05-16 11:56:51 +0300

    Added A3

---
+++ MySQL/MySQL07-DataAggregation/A3_MinimumSalary.sql
@@ -0,0 +1,8 @@
+#Write a query to retrieve information about the departments grouped by department_id with minimum salary higher than 800.
+#Round the salary result to two digits after the decimal point.
+#Submit your queries with the MySQL prepare DB & run queries strategy.
+SELECT `department_id`, ROUND(MIN(`salary`), 2) AS `Min Salary`
+FROM `employees`
+WHERE `salary` > 800
+GROUP BY `department_id`
+LIMIT 1;
