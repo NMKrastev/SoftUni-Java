1293022a89be46e1b4c00483d4b7073f9ec40cab
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2023-05-16 12:09:37 +0300

    Added A3

--- MySQL/MySQL07-DataAggregation/A3_MinimumSalary.sql
+++ MySQL/MySQL07-DataAggregation/A3_MinimumSalary.sql
@@ -3,6 +3,5 @@
 #Submit your queries with the MySQL prepare DB & run queries strategy.
 SELECT `department_id`, ROUND(MIN(`salary`), 2) AS `Min Salary`
 FROM `employees`
-WHERE `salary` > 800
 GROUP BY `department_id`
-LIMIT 1;
+HAVING `Min Salary` >
