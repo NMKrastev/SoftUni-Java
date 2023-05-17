0feaade89b42452c7e90b6ba6301ad46a29211b2
Nikola <nikola.malinov.krastev@gmail.com>
2023-05-17 14:29:45 +0300

    Added A2

---
+++ MySQL/MySQL10-Exercise_TableRelations/A2_OneToManyRelationship.sql
@@ -0,0 +1,44 @@
+#Create two tables as follows. Use appropriate data types.
+#                manufacturers
+#manufacturer_id     name    established_on
+#1                   BMW     01/03/1916
+#2                   Tesla   01/01/2003
+#3                   Lada    01/05/1966
+
+#            models
+#model_id    name       manufacturer_id
+#101         X1         1
+#102         i6         1
+#103         Model S    2
+#104         Model X    2
+#105         Model 3    2
+#106         Nova       3
+CREATE TABLE `manufacturers`
+(
+    `manufacturer_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name`            VARCHAR(50) NOT NULL UNIQUE,
+    `established_on`  DATE
+);
+
+INSERT INTO `manufacturers`(`manufacturer_id`, `name`, `established_on`)
+VALUES (1, 'BMW', '1916/03/01'),
+       (2, 'Tesla', '2003/01/01'),
+       (3, 'Lada', '1966/05/01');
+
+CREATE TABLE `models`
+(
+    `model_id`        INT PRIMARY KEY AUTO_INCREMENT,
+    `name`            VARCHAR(50) NOT NULL UNIQUE,
+    `manufacturer_id` INT,
+    CONSTRAINT fk_models_manufacturers
+        FOREIGN KEY (`manufacturer_id`)
+            REFERENCES `manufacturers` (`manufacturer_id`)
+);
+
+INSERT INTO `models`(`model_id`, `name`, `manufacturer_id`)
+VALUES (101, 'X1', 1),
+       (102, 'i6', 1),
+       (103, 'Model S', 2),
+       (104, 'Model X', 2),
+       (105, 'Model 3', 2),
+       (106, 'Nova', 3);
