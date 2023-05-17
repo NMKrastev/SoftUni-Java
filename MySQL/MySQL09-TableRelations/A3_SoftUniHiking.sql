532371424e1765f9b46c759ad4d7963afd705fcb
Nikola <nikola.malinov.krastev@gmail.com>
2023-05-17 12:30:48 +0300

    Added A3

---
+++ MySQL/MySQL09-TableRelations/A3_SoftUniHiking.sql
@@ -0,0 +1,9 @@
+#Get information about the hiking routes – starting point and ending point, and their leaders – name and id.
+#Submit your queries using the "MySQL prepare DB and Run Queries" strategy.
+SELECT r.`starting_point` AS `route_starting_point`,
+       r.`end_point` AS `route_ending_point`,
+       r.`leader_id`,
+       CONCAT_WS(' ', c.`first_name`, c.`last_name`) AS `leader_name`
+FROM `routes` AS r
+    JOIN `campers` AS c
+        ON r.`leader_id` = c.`id`;
