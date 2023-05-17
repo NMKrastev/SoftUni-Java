a06af4e9d118498b856df718ff7250c4d22c9e4f
Nikola <nikola.malinov.krastev@gmail.com>
2023-05-17 12:01:10 +0300

    Added A2

--- MySQL/MySQL09-TableRelations/A2_TripOrganization.sql
+++ MySQL/MySQL09-TableRelations/A2_TripOrganization.sql
@@ -1,3 +1,8 @@
 #Write a query to retrieve information about SoftUni camp's transportation organization.
 #Get information about the drivers (name and id) and their vehicle type.
 #Submit your queries using the "MySQL prepare DB and Run Queries" strategy.
+SELECT v.`driver_id`,
+       v.`vehicle_type`,
+       CONCAT_WS(' ', c.first_name, c.last_name) AS `driver_name`
+FROM `vehicles` AS `v`
+         JOIN `campers` AS `c` ON v.driver_id = c.id;
