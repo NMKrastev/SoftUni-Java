dccc36d97ad0b421a1f037bd737f9f07050bba0e
Nikola <nikola.malinov.krastev@gmail.com>
2023-05-18 16:41:57 +0300

    Added A6

---
+++ MySQL/MySQL12-Exercise_SubqueriesAndJOINs/A6_EmployeesHiredAfter.sql
@@ -0,0 +1,15 @@
+#Write a query that selects:
+#• first_name
+#• last_name
+#• hire_date
+#• dept_name
+#Filter only employees hired after 1/1/1999 and from either the "Sales" or the "Finance" departments.
+#Sort the result by hire_date (ascending).
+SELECT e.`first_name`,
+       e.`last_name`,
+       e.`hire_date`,
+       d.`name` AS `dept_name`
+FROM `employees` AS e
+JOIN `departments` AS d ON d.`department_id` = e.`department_id`
+WHERE e.`hire_date` > '1999-01-01' AND d.`name` IN ('Sales', 'Finance')
+ORDER BY e.`hire_date`;
