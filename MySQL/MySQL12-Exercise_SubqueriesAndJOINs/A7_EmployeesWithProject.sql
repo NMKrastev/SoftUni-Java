39f0b73d2089e56a4a669377765b3d7a6fc03198
Nikola <nikola.malinov.krastev@gmail.com>
2023-05-18 17:03:40 +0300

    Added A7

---
+++ MySQL/MySQL12-Exercise_SubqueriesAndJOINs/A7_EmployeesWithProject.sql
@@ -0,0 +1,15 @@
+#Write a query that selects:
+#• employee_id
+#• first_name
+#• project_name
+#Filter only employees with a project, which has started after 13.08.2002 and it is still ongoing (no end date).
+#Return the first 5 rows sorted by first_name then by project_name both in ascending order.
+SELECT e.`employee_id`,
+       e.`first_name`,
+       p.`name` AS `project_name`
+FROM `employees` AS e
+JOIN `employees_projects` AS ep ON ep.`employee_id` = e.`employee_id`
+JOIN `projects` AS p ON p.`project_id` = ep.`project_id`
+WHERE DATE(p.`start_date`) > '2002-08-13' AND p.`end_date` IS NULL
+ORDER BY e.`first_name`, p.`name`
+LIMIT 5;
