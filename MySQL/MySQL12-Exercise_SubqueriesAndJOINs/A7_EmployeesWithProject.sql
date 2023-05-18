12da8edcc0753f7800794821e40a8d19a6aba465
Nikola <nikola.malinov.krastev@gmail.com>
2023-05-18 17:04:20 +0300

    Added A7

--- MySQL/MySQL12-Exercise_SubqueriesAndJOINs/A7_EmployeesWithProject.sql
+++ MySQL/MySQL12-Exercise_SubqueriesAndJOINs/A7_EmployeesWithProject.sql
@@ -8,8 +8,11 @@ SELECT e.`employee_id`,
        e.`first_name`,
        p.`name` AS `project_name`
 FROM `employees` AS e
-JOIN `employees_projects` AS ep ON ep.`employee_id` = e.`employee_id`
-JOIN `projects` AS p ON p.`project_id` = ep.`project_id`
-WHERE DATE(p.`start_date`) > '2002-08-13' AND p.`end_date` IS NULL
+         JOIN `employees_projects` AS ep
+              ON ep.`employee_id` = e.`employee_id`
+         JOIN `projects` AS p
+              ON p.`project_id` = ep.`project_id`
+WHERE DATE(p.`start_date`) > '2002-08-13'
+  AND p.`end_date` IS NULL
 ORDER BY e.`first_name`, p.`name`
 LIMIT 5;
