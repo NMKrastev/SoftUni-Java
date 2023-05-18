cad55e0c2d36fc7fe931c34244cb4df7dc420014
Nikola <nikola.malinov.krastev@gmail.com>
2023-05-18 16:16:55 +0300

    Added A3

---
+++ MySQL/MySQL12-Exercise_SubqueriesAndJOINs/A3_SalesEmployee.sql
@@ -0,0 +1,12 @@
+#Write a query that selects:
+#• employee_id
+#• first_name
+#• last_name
+#• department_name
+#Sort the result by employee_id in descending order.
+#Select only employees from the "Sales" department.
+SELECT e.`employee_id`, e.`first_name`, e.`last_name`, d.`name` AS `department_name`
+FROM `employees` AS e
+JOIN `departments` AS d ON e.`department_id` = d.`department_id`
+WHERE d.`name` = 'Sales'
+ORDER BY employee_id DESC;
