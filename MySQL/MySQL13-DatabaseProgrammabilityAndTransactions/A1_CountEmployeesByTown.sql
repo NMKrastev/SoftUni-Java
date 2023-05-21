b9f4f0f8d257f3b5376bb0dc38a24fc90b97d2d7
Nikola <nikola.malinov.krastev@gmail.com>
2023-05-21 12:19:52 +0300

    Added A1

--- MySQL/MySQL13-DatabaseProgrammabilityAndTransactions/A1_CountEmployeesByTown.sql
+++ MySQL/MySQL13-DatabaseProgrammabilityAndTransactions/A1_CountEmployeesByTown.sql
@@ -8,10 +8,11 @@ CREATE FUNCTION ufn_count_employees_by_town(town_name VARCHAR(50))
     DETERMINISTIC
 BEGIN
     DECLARE count_by_town INT;
-    SET count_by_town := (SELECT COUNT(*) FROM `employees` AS e
-                                          JOIN `addresses` AS a ON a.`address_id` = e.`address_id`
-                                          JOIN `towns` AS t ON t.`town_id` = a.`town_id`
-                                          WHERE t.`name` = town_name);
+    SET count_by_town := (SELECT COUNT(*)
+                          FROM `employees` AS e
+                                   JOIN `addresses` AS a ON a.`address_id` = e.`address_id`
+                                   JOIN `towns` AS t ON t.`town_id` = a.`town_id`
+                          WHERE t.`name` = town_name);
     RETURN count_by_town;
 END $$
 
---
+++ MySQL/MySQL13-DatabaseProgrammabilityAndTransactions/A2_EmployeesPromotion.sql
@@ -0,0 +1,3 @@
+#Write a stored procedure usp_raise_salaries(department_name) to raise the salary of all employees
+#in given department as parameter by 5%.
+#Submit your queries using the "MySQL Run Skeleton, run queries and check DB" strategy.
