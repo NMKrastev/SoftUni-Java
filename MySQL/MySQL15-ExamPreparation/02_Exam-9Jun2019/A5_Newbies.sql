78aaeaea879631954773ce6a9b39662e67deb329
Nikola <nikola.malinov.krastev@gmail.com>
2023-05-23 11:02:13 +0300

    Added A5

---
+++ MySQL/MySQL15-ExamPreparation/02_Exam-9Jun2019/A5_Newbies.sql
@@ -0,0 +1,7 @@
+SELECT `id`,
+       CONCAT_WS(' ', `first_name`, `last_name`) AS `full_name`,
+       CONCAT('$', `salary`) AS `salary`,
+       `started_on`
+FROM `employees`
+WHERE `salary` >= 100000 AND `started_on` > '2018-01-01'
+ORDER BY `salary` DESC, `id`;
