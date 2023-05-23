56d0795f7a0079866413e9261069957e881b6efe
Nikola <nikola.malinov.krastev@gmail.com>
2023-05-23 11:08:58 +0300

    Added A5

--- MySQL/MySQL15-ExamPreparation/02_Exam-9Jun2019/A5_Newbies.sql
+++ MySQL/MySQL15-ExamPreparation/02_Exam-9Jun2019/A5_Newbies.sql
@@ -1,7 +1,8 @@
 SELECT `id`,
        CONCAT_WS(' ', `first_name`, `last_name`) AS `full_name`,
-       CONCAT('$', `salary`) AS `salary`,
+       CONCAT('$', `salary`)                     AS `salary`,
        `started_on`
 FROM `employees`
-WHERE `salary` >= 100000 AND `started_on` > '2018-01-01'
+WHERE `salary` >= 100000
+  AND `started_on` > '2018-01-01'
 ORDER BY `salary` DESC, `id`;
