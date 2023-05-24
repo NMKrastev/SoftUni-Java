543f368fbde39b22ad0f90df0508073e67882ffd
Nikola <nikola.malinov.krastev@gmail.com>
2023-05-24 10:42:17 +0300

    Added A3

---
+++ MySQL/MySQL15-ExamPreparation/03_Exam-9Feb2020/A3_Update.sql
@@ -0,0 +1,5 @@
+UPDATE `coaches` AS c
+SET c.`coach_level` = c.`coach_level` + 1
+WHERE c.`id` IN (SELECT DISTINCT `coach_id`
+                 FROM `players_coaches`)
+  AND c.`first_name` LIKE 'A%';
