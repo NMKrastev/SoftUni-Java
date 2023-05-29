5287ad3f9bebb9a45fc5e73d879013cb244a4d86
Nikola <nikola.malinov.krastev@gmail.com>
2023-05-29 10:27:18 +0300

    Added A2

---
+++ MySQL/MySQL15-ExamPreparation/06_Exam-20Jun2021/A2_Insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO `clients`(`full_name`, `phone_number`)
+SELECT CONCAT_WS(' ', `first_name`, `last_name`),
+       CONCAT('(088) 9999', `id` * 2)
+FROM `drivers`
+WHERE `id` BETWEEN 10 AND 20;
