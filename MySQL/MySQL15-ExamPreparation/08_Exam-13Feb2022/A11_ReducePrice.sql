d91147141e650cd9df5d5a5a03e733aebb16c364
Nikola <nikola.malinov.krastev@gmail.com>
2023-06-02 10:19:41 +0300

    Added A11

---
+++ MySQL/MySQL15-ExamPreparation/08_Exam-13Feb2022/A11_ReducePrice.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+
+CREATE PROCEDURE udp_reduce_price(`category_name` VARCHAR(50))
+
+BEGIN
+
+    UPDATE `products` AS p
+        JOIN `reviews` AS r ON r.`id` = p.`review_id`
+        JOIN `categories` AS c ON c.`id` = p.`category_id`
+    SET p.`price` = p.`price` * 0.7
+    WHERE r.`rating` < 4
+      AND c.`name` = `category_name`;
+
+END $$
