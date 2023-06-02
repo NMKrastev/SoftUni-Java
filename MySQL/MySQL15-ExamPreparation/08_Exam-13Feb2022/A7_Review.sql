d789233b8e74f2a2bf24a72814c66c886d0d2301
Nikola <nikola.malinov.krastev@gmail.com>
2023-06-02 09:34:23 +0300

    Added A7

---
+++ MySQL/MySQL15-ExamPreparation/08_Exam-13Feb2022/A7_Review.sql
@@ -0,0 +1,9 @@
+SELECT `id`,
+       `content`,
+       `rating`,
+       `picture_url`,
+       `published_at`
+FROM reviews
+WHERE `content` LIKE 'My%'
+  AND CHAR_LENGTH(`content`) > 61
+ORDER BY `rating` DESC;
