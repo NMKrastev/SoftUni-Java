1c94d58058643c35a1e223dbdefdb67436b3a00b
Nikola <nikola.malinov.krastev@gmail.com>
2023-06-05 09:18:42 +0300

    Added A9

---
+++ MySQL/MySQL15-ExamPreparation/09_Exam-10Apr2022/A9_RatingSystem.sql
@@ -0,0 +1,11 @@
+SELECT m.`title`,
+       (CASE
+            WHEN mai.`rating` <= 4 THEN 'poor'
+            WHEN mai.`rating` <= 7 THEN 'good'
+            ELSE 'excellent'
+           END)                                    AS `rating`,
+       IF(mai.`has_subtitles` = 0, '-', 'english') AS `subtitles`,
+       mai.`budget`
+FROM movies AS m
+         JOIN `movies_additional_info` AS mai ON mai.`id` = m.`movie_info_id`
+ORDER BY mai.`budget` DESC;
