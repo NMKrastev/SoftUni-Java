f4a13c6a44fe731eba4193406ca1df738f2696b5
Nikola <nikola.malinov.krastev@gmail.com>
2023-06-06 07:57:29 +0300

    Added A5

---
+++ MySQL/MySQL15-ExamPreparation/10_Exam-15Oct2022/A6_Birthdate.sql
@@ -0,0 +1,10 @@
+SELECT `first_name`,
+       `last_name`,
+       `birthdate`,
+       `review`
+FROM clients
+WHERE `card` IS NULL
+  AND YEAR(`birthdate`) BETWEEN 1978 AND 1993
+ORDER BY `last_name` DESC,
+         id
+LIMIT 5;
