14e0d6f9557480f7c64ebe258a8eaa3e5b3abaac
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2023-06-13 21:08:25 +0300

    Added A1

--- SpringData/SD01_DBAppsIntroduction/src/A1_WritingYourOwnDataRetrievalApplication.java
+++ SpringData/SD01_DBAppsIntroduction/src/A1_WritingYourOwnDataRetrievalApplication.java
@@ -3,31 +3,26 @@ import java.util.Properties;
 import java.util.Scanner;
 
 public class A1_WritingYourOwnDataRetrievalApplication {
+
+    private static final String GET_USER_GAMES_COUNT_BY_USERNAME = "SELECT u.`first_name`, u.`last_name`, " +
+            "(SELECT COUNT(`user_id`) FROM `users_games` WHERE `user_id` = u.`id`) AS `games_count` " +
+            "FROM `users` AS u " +
+            "WHERE u.`user_name` = ?";
+
     public static void main(String[] args) throws SQLException {
 
-        Properties properties = new Properties();
-        properties.setProperty("user", "root");
-        properties.setProperty("password", "root_12345");
+        Connection connection = getConnection();
 
-        Connection connection = DriverManager
-                .getConnection("jdbc:mysql://localhost:3306/diablo", properties);
+        String username = getUsername();
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Please enter username: ");
-        String username = scanner.nextLine();
-
-        PreparedStatement stmt =
-                connection.prepareStatement
-                        ("SELECT u.`first_name`, u.`last_name`, " +
-                                    "(SELECT COUNT(`user_id`) FROM `users_games` WHERE `user_id` = u.`id`) AS `games_count` " +
-                                "FROM `users` AS u " +
-                                "WHERE u.`user_name` = ?");
-
-        stmt.setString(1, username);
+        PreparedStatement stmt = getPreparedStatement(connection, username);
 
         ResultSet rs = stmt.executeQuery();
 
+        printResults(username, rs);
+    }
+
+    private static void printResults(String username, ResultSet rs) throws SQLException {
         if (rs.next()) {
 
             String dbFirstName = rs.getString("first_name");
@@ -39,5 +34,31 @@ public class A1_WritingYourOwnDataRetrievalApplication {
         } else {
             System.out.println("No such user exists");
         }
+    }
+
+    private static PreparedStatement getPreparedStatement(Connection connection, String username) throws SQLException {
+        PreparedStatement stmt =
+                connection.prepareStatement(GET_USER_GAMES_COUNT_BY_USERNAME);
+
+        stmt.setString(1, username);
+        return stmt;
+    }
+
+    private static String getUsername() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter username: ");
+        String username = scanner.nextLine();
+        return username;
+    }
+
+    private static Connection getConnection() throws SQLException {
+
+        Properties properties = new Properties();
+        properties.setProperty("user", "root");
+        properties.setProperty("password", "root_12345");
+
+        Connection connection = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/diablo", properties);
+        return connection;
     }
 }
