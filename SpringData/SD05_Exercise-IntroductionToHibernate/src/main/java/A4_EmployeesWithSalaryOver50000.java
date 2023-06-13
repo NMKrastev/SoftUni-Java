f038493a071b494d5cb76b73440d0bba61c17a24
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2023-06-13 18:21:15 +0300

    Added A4

--- SpringData/SD05_Exercise-IntroductionToHibernate/src/main/java/A4_EmployeesWithSalaryOver50000.java
+++ SpringData/SD05_Exercise-IntroductionToHibernate/src/main/java/A4_EmployeesWithSalaryOver50000.java
@@ -13,13 +13,13 @@ public class A4_EmployeesWithSalaryOver50000 {
 
     public static void main(String[] args) {
 
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        final EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
 
-        EntityManager manager = factory.createEntityManager();
+        final EntityManager manager = factory.createEntityManager();
 
         manager.getTransaction().begin();
 
-        List<Employee> employeeList = manager.createQuery(GET_ALL_EMPLOYEES_WITH_SALARY_OVER_50000, Employee.class)
+        final List<Employee> employeeList = manager.createQuery(GET_ALL_EMPLOYEES_WITH_SALARY_OVER_50000, Employee.class)
                 .getResultList();
 
         employeeList.forEach(e -> System.out.println(e.getFirstName()));
