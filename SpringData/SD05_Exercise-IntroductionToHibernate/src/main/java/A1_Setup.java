e5e6b7442148d8a1803d4debc2080d4a56b1a1c8
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2023-06-13 18:22:39 +0300

    Added A1

--- SpringData/SD05_Exercise-IntroductionToHibernate/src/main/java/A1_Setup.java
+++ SpringData/SD05_Exercise-IntroductionToHibernate/src/main/java/A1_Setup.java
@@ -2,8 +2,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
-import java.util.List;
-
 public class A1_Setup {
     public static void main(String[] args) {
 
