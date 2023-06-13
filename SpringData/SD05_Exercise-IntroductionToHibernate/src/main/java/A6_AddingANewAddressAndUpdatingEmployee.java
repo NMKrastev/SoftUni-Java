cb8f6447fc493cb388a664030cc49ac82a3b8008
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2023-06-13 17:22:21 +0300

    Added A6

---
+++ SpringData/SD05_Exercise-IntroductionToHibernate/src/main/java/A6_AddingANewAddressAndUpdatingEmployee.java
@@ -0,0 +1,39 @@
+import entities.Address;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.Scanner;
+
+public class A6_AddingANewAddressAndUpdatingEmployee {
+
+    private static final String PERSISTENCE_UNIT_NAME = "soft_uni";
+    private static final String SET_ADDRESS_TO_EMPLOYEE_WITH_LAST_NAME =
+            "UPDATE Employee AS e SET e.address = (SELECT a.id FROM Address AS a WHERE a.text = 'Vitoshka 15') WHERE e.lastName = ?1";
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+
+        EntityManager manager = factory.createEntityManager();
+
+        manager.getTransaction().begin();
+
+        Address address = new Address();
+        address.setText("Vitoshka 15");
+        manager.persist(address);
+
+        System.out.println("Please, enter last name: ");
+        String lastName = scanner.nextLine();
+
+        manager.createQuery(SET_ADDRESS_TO_EMPLOYEE_WITH_LAST_NAME)
+                .setParameter(1, lastName).executeUpdate();
+
+        manager.getTransaction().commit();
+
+        manager.close();
+        factory.close();
+    }
+}
