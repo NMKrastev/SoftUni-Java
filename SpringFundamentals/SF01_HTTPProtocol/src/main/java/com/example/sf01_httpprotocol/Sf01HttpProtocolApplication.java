b7b35cc468615e2e5e06474678a1555f203bb2de
Nikola <nikola.malinov.krastev@gmail.com>
2023-09-13 16:09:05 +0300

    Added SF01_HTTPProtocol

---
+++ SpringFundamentals/SF01_HTTPProtocol/src/main/java/com/example/sf01_httpprotocol/Sf01HttpProtocolApplication.java
@@ -0,0 +1,13 @@
+package com.example.sf01_httpprotocol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sf01HttpProtocolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sf01HttpProtocolApplication.class, args);
+	}
+
+}
---
+++ SpringFundamentals/SF01_HTTPProtocol/src/test/java/com/example/sf01_httpprotocol/Sf01HttpProtocolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sf01_httpprotocol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sf01HttpProtocolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
