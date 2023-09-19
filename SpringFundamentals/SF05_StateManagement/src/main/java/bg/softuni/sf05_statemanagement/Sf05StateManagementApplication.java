3d7a460587eeafc893cc36f9db132b393f514c12
Nikola <nikola.malinov.krastev@gmail.com>
2023-09-19 13:10:11 +0300

    Added SF05 State Management

---
+++ SpringFundamentals/SF05_StateManagement/src/main/java/bg/softuni/sf05_statemanagement/Sf05StateManagementApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.sf05_statemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sf05StateManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sf05StateManagementApplication.class, args);
+	}
+
+}
---
+++ SpringFundamentals/SF05_StateManagement/src/test/java/bg/softuni/sf05_statemanagement/Sf05StateManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.sf05_statemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sf05StateManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
