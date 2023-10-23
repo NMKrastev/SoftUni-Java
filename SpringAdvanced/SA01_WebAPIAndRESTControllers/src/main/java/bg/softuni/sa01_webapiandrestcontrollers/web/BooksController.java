53e4e02a1d37c453c14a6884f25e8be5dd0e9a49
Nikola <nikola.malinov.krastev@gmail.com>
2023-10-23 22:30:27 +0300

    Added JS client for fetching all books and deleting a book

--- SpringAdvanced/SA01_WebAPIAndRESTControllers/src/main/java/bg/softuni/sa01_webapiandrestcontrollers/web/BooksController.java
+++ SpringAdvanced/SA01_WebAPIAndRESTControllers/src/main/java/bg/softuni/sa01_webapiandrestcontrollers/web/BooksController.java
@@ -9,6 +9,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/books")
 public class BooksController {
